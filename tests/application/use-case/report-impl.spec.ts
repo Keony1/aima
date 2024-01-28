887771c051cddedf01d737bd53ae8b68d59c3d2b
Keony Schröer <keony.schroer@gmail.com>
2024-01-28 09:39:17 -0300

    test(report): functionality

---
+++ tests/application/use-case/report-impl.spec.ts
@@ -0,0 +1,31 @@
+import { ReportRepository } from "../../../src/application/protocols/database/report-repository";
+import { ReportImpl } from "../../../src/application/use-cases/report-impl";
+import { RestockingNeeds } from "../../../src/domain/entities";
+
+const repository: jest.Mocked<ReportRepository> = {
+  summary: jest.fn(),
+};
+
+describe("ReportImpl", () => {
+  let sut: ReportImpl;
+
+  beforeEach(() => {
+    sut = new ReportImpl(repository);
+  });
+
+  describe("summary", () => {
+    it('should call "repository" with correct args', async () => {
+      await sut.generateReport(RestockingNeeds.Low);
+
+      expect(repository.summary).toHaveBeenCalledWith(RestockingNeeds.Low);
+    });
+
+    it('should call throw when "repository" throws', async () => {
+      repository.summary.mockRejectedValueOnce(new Error("summary error"));
+
+      const promise = sut.generateReport(RestockingNeeds.Low);
+
+      await expect(promise).rejects.toThrow("summary error");
+    });
+  });
+});
