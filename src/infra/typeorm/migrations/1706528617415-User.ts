bf6ee433d0eab3996735fa0d18f10f968a3f1168
Keony Schröer <keony.schroer@gmail.com>
2024-01-30 07:52:00 -0300

    feat(user): create a default user

--- src/infra/typeorm/migrations/1706528617415-User.ts
+++ src/infra/typeorm/migrations/1706528617415-User.ts
@@ -10,12 +10,12 @@ export class User1706528617415 implements MigrationInterface {
     `);
 
     queryRunner.query(`
-        INSERT INTO users 
-          (username, password) 
-        values 
-          ('user', 
-          'c8b2505b76926abdc733523caa9f439142f66aa7293a7baaac0aed41a191eef6');
-    `);
+      INSERT INTO users
+         (username, password)
+       values
+         ('user',
+         'c8b2505b76926abdc733523caa9f439142f66aa7293a7baaac0aed41a191eef6');
+      `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
